3870c01f703396d610531a71a5b66ee52cad8bad
RAJ <vermar4142@gmail.com>
2020-08-20 22:32:49 +0530

    Added:- The functionality for noplay,snake and ladder option

--- snakeladdersimulation.sh
+++ snakeladdersimulation.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
-echo "Welcome to the snake and ladder simulator program"
+echo -e "Welcome to the snake and ladder simulator program \n"
 
 #Snake and Ladder game played with single player at start position 0(UC1).
 
-#variables
+#constant
 startPosition=0
 
 #The Player rolls the die to get a number between 1 to 6(UC2).
 rollDie=$(($RANDOM % 6 + 1))
-echo $rollDie
+
+#The Player checks for a Option For No Play, Ladder or Snake.(UC3).
+checkOption=$(($RANDOM % 3))
+playerPosition=$startPosition
+
+if [[ $checkOption -eq 0 ]]
+then
+	echo -e "No Play Option \n"
+	playerPosition=$playerPosition
+elif [[ $checkOption -eq 1 ]]
+then
+	echo -e "Ladder Option \n"
+	playerPosition=$((playerPosition + rollDie))
+elif [[ $checkOption -eq 2 ]]
+then
+	echo -e "Snake Option \n"
+	currentPosition=$(($playerPosition - $rollDie))
+	if [ $currentPosition -gt 0 ]
+	then
+		playerPosition=$((playerPosition - rollDie))
+	else
+		playerPosition=$playerPosition
+	fi
+fi
+echo "Die Rolled With Value of : "$rollDie
+echo "The Player Current Position After Die Rolled : "$playerPosition
